3dae30baf7080471551085d55302bd7ebb9776fe
chalik <chalikfroza@gmail.com>
2021-04-18 13:48:20 +0700

    not dont yet 2

--- src/pages/ChatBot/index.js
+++ src/pages/ChatBot/index.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View, StatusBar, SafeAreaView} from 'react-native';
 import colors from '../../assets/colors';
 import {logo2, Option1, Option2, Option3} from '../../assets';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import { color } from 'react-native-reanimated';
 
 const ChatBot = ({navigation}) => {
   return (
@@ -10,6 +11,21 @@ const ChatBot = ({navigation}) => {
       <StatusBar translucent backgroundColor="white" barStyle="dark-content" />
       <View style={styles.wrapper}>
         <Text style={styles.Title}>Konsultasi</Text>
+        <Text style={styles.Text}>
+          Konsultasi dengan beberapa pilihan dibawah
+        </Text>
+        <View style={{marginVertical: 20}}>
+          <TouchableOpacity onPress={()=>{navigation.navigate('Konsultasi')}}>
+            <View style={styles.Box}>
+              <Text style={styles.BoxText}>Chat Bot</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>{navigation.navigate('Konsultasi')}}>
+            <View style={styles.Box}>
+              <Text style={styles.BoxText}>Chat Dokter</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -28,6 +44,23 @@ const styles = StyleSheet.create({
   Title: {
     fontFamily: 'Karla-Bold',
     fontSize: 24,
+  },
+  Text: {
+    fontFamily: 'Karla-Regular',
+    fontSize: 16,
+  },
+  Box: {
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    marginVertical:5,
+    borderColor:colors.yellow,
+  },
+  BoxText: {
+    marginVertical: 20,
+    fontFamily:'Karla-Bold',
+    color:colors.gray_dark
   },
 });
 
